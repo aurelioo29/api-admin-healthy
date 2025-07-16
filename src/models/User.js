3cbc0b6165ce14fb9c4898f6f928e8fdd19e4616
aurelioo29 <aureliolucio5@gmail.com>
2025-07-16 10:17:50 +0700

    fix: Typo enum on stase

--- src/models/User.js
+++ src/models/User.js
@@ -37,7 +37,7 @@ const User = sequelize.define(
         "radiologi",
         "paru",
         "anak",
-        "sikiatri",
+        "psikiatri",
         "neurologi",
         "forensik",
         "bedah",
