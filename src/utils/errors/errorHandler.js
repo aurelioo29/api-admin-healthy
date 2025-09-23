fa4891503e435d7662f3cf35339c98cfbe2105df
aurelioo29 <aureliolucio5@gmail.com>
2025-09-23 14:17:38 +0700

    refac: update handler error on multer

--- src/utils/errors/errorHandler.js
+++ src/utils/errors/errorHandler.js
@@ -1,11 +1,22 @@
-const errorHandler = (error, request, response, next) => {
-  const code = response.code ? response.code : 500;
+const multer = require("multer");
 
-  response.status(code).json({
+const errorHandler = (error, req, res, next) => {
+  let code = res.statusCode && res.statusCode !== 200 ? res.statusCode : 500;
+
+  if (error instanceof multer.MulterError) {
+    code = 400;
+  }
+
+  // Tangani error dari mimetype (image validation)
+  if (error.message && error.message.includes("Invalid file type")) {
+    code = 400;
+  }
+
+  res.status(code).json({
     code,
     success: false,
     message: error.message || "Internal Server Error",
-    stack: error.stack,
+    stack: process.env.NODE_ENV === "production" ? undefined : error.stack,
   });
 };
 
