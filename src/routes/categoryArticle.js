dcc9cf0435d031bb2adc4c5473eda31d019991c3
aurelioo29 <aureliolucio5@gmail.com>
2025-09-29 23:28:04 +0700

    fix: bug variable on category Article

--- src/routes/categoryArticle.js
+++ src/routes/categoryArticle.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const categoryArticleController = require("../controllers/categoryArticle");
+const { categoryArticleController } = require("../controllers");
 const rateLimiter = require("../middlewares/rateLimiter");
 const validate = require("../utils/validators/validate");
 const {
